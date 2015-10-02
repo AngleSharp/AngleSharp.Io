d18aa1f97a5a521b35206e2dfc895c7124373508
Florian Rappl <rappl.florian@gmail.com>
2015-10-02 23:27:11 +0200

    Added VS solution / project skeletons

---
+++ AngleSharp.Io/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AngleSharp.Io")]
+[assembly: AssemblyDescription("Extension for AngleSharp to provide additional requesters and storage options.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AngleSharp")]
+[assembly: AssemblyProduct("AngleSharp.Io")]
+[assembly: AssemblyCopyright("Copyright © AngleSharp, 2013-2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("10d96c84-41d5-4c21-bacf-5d6997c85082")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: InternalsVisibleToAttribute("AngleSharp.Io.Tests")]
