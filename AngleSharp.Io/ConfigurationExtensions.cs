3fd2de64b2138e290077efa5fec404937dbbfc9d
Florian Rappl <rappl.florian@gmail.com>
2016-01-05 21:56:07 +0100

    Removed the duplicate loader

--- AngleSharp.Io/ConfigurationExtensions.cs
+++ AngleSharp.Io/ConfigurationExtensions.cs
@@ -1,11 +1,10 @@
 ﻿namespace AngleSharp
 {
     using AngleSharp.Io.Network;
-    using AngleSharp.Io.Services;
     using AngleSharp.Network;
     using AngleSharp.Network.Default;
-    using AngleSharp.Services;
-    using System.Linq;
+    using AngleSharp.Services.Default;
+    using System;
     using System.Net.Http;
 
     /// <summary>
@@ -17,37 +16,28 @@
         /// Adds a loader service that comes with all (improved) requesters.
         /// </summary>
         /// <param name="configuration">The configuration to use.</param>
+        /// <param name="setup">Optional setup for the loader service.</param>
         /// <returns>The new configuration.</returns>
-        public static IConfiguration WithRequesters(this IConfiguration configuration)
+        public static IConfiguration WithRequesters(this IConfiguration configuration, Action<LoaderService> setup = null)
         {
-            if (!configuration.Services.OfType<ILoaderService>().Any())
-            {
-                var requesters = new IRequester [] { new HttpClientRequester(), new DataRequester() };
-                var service = new LoaderService(requesters);
-                return configuration.With(service);
-            }
-
-            return configuration;
+            var requesters = new IRequester[] { new HttpClientRequester(), new DataRequester() };
+            return configuration.WithDefaultLoader(setup, requesters);
         }
-
 
         /// <summary>
         /// Adds a loader service that comes with all (improved) requesters.
         /// </summary>
         /// <param name="configuration">The configuration to use.</param>
-        /// <param name="httpMessageHandler">The HTTP handler stack to use for sending requests.</param>
+        /// <param name="httpMessageHandler">
+        /// The HTTP handler stack to use for sending requests.
+        /// </param>
+        /// <param name="setup">Optional setup for the loader service.</param>
         /// <returns>The new configuration.</returns>
-        public static IConfiguration WithHttpRequesters(this IConfiguration configuration, HttpMessageHandler httpMessageHandler)
+        public static IConfiguration WithRequesters(this IConfiguration configuration, HttpMessageHandler httpMessageHandler, Action<LoaderService> setup = null)
         {
-            if (!configuration.Services.OfType<ILoaderService>().Any())
-            {
-                var httpClient = new HttpClient(httpMessageHandler);
-                var requesters = new IRequester[] { new HttpClientRequester(httpClient), new DataRequester() };
-                var service = new LoaderService(requesters);
-                return configuration.With(service);
-            }
-
-            return configuration;
+            var httpClient = new HttpClient(httpMessageHandler);
+            var requesters = new IRequester[] { new HttpClientRequester(httpClient), new DataRequester() };
+            return configuration.WithDefaultLoader(setup, requesters);
         }
     }
 }
