d2cca6ac91cab8f492ca5caef983faa77525c127
Florian Rappl <rappl.florian@gmail.com>
2016-05-06 12:57:30 +0200

    Implemented the File requester #11

---
+++ src/AngleSharp.Io.Tests/Network/FileRequesterTests.cs
@@ -0,0 +1,63 @@
+﻿namespace AngleSharp.Io.Tests.Network
+{
+    using AngleSharp.Dom.Html;
+    using AngleSharp.Extensions;
+    using AngleSharp.Io.Network;
+    using AngleSharp.Network.Default;
+    using NUnit.Framework;
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class FileRequesterTests
+    {
+        static String GetLocalPath()
+        {
+            var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var path = Path.Combine(directory, "TestContent.txt");
+            var uri = new Uri(path);
+            return uri.AbsoluteUri;
+        }
+
+        [Test]
+        public async Task GetLocalFileViaFileRequester()
+        {
+            var url = GetLocalPath();
+            var requester = new FileRequester();
+            var request = new Request { Address = Url.Create(url) };
+
+            var response = await requester.RequestAsync(request, CancellationToken.None);
+            Assert.IsNotNull(response);
+
+            var content = await new StreamReader(response.Content).ReadToEndAsync();
+            Assert.AreEqual("Foo!", content);
+        }
+
+        [Test]
+        public async Task FollowLinkToUseFileRequesterUsingAllRequesters()
+        {
+            var url = GetLocalPath();
+            var config = Configuration.Default.WithRequesters();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(res => res.Content("<a href='" + url + "'>Download</a>"));
+            var result = await document.QuerySelector<IHtmlAnchorElement>("a").NavigateAsync();
+            var content = result.Body.TextContent;
+            Assert.AreEqual("Foo!", content);
+        }
+
+        [Test]
+        public async Task FollowLinkToUseFileRequesterUsingStandardRequesters()
+        {
+            var url = GetLocalPath();
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(res => res.Content("<a href='" + url + "'>Download</a>"));
+            var result = await document.QuerySelector<IHtmlAnchorElement>("a").NavigateAsync();
+            var content = result.Body.TextContent;
+            Assert.AreEqual(0, content.Length);
+        }
+    }
+}
---
+++ src/AngleSharp.Io/Network/FileRequester.cs
@@ -0,0 +1,37 @@
+﻿namespace AngleSharp.Io.Network
+{
+    using AngleSharp.Network;
+    using AngleSharp.Network.Default;
+    using System;
+    using System.Net;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class FileRequester : IRequester
+    {
+        public async Task<IResponse> RequestAsync(IRequest request, CancellationToken cancel)
+        {
+            var requester = FileWebRequest.Create(request.Address.Href) as FileWebRequest;
+
+            if (requester != null)
+            {
+                var response = await requester.GetResponseAsync().ConfigureAwait(false);
+                var content = response.GetResponseStream();
+
+                return new Response
+                {
+                    Address = request.Address,
+                    Content = content,
+                    StatusCode = HttpStatusCode.OK
+                };
+            }
+
+            return default(IResponse);
+        }
+
+        public Boolean SupportsProtocol(String protocol)
+        {
+            return !String.IsNullOrEmpty(protocol) && protocol.Equals(ProtocolNames.File);
+        }
+    }
+}
