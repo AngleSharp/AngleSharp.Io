7ba846d7bc367ef1c5128472b9c3a68bdd422b69
Florian Rappl <rappl.florian@gmail.com>
2016-05-06 12:59:39 +0200

    Documentation comment for the File requester #11

--- src/AngleSharp.Io/Network/FileRequester.cs
+++ src/AngleSharp.Io/Network/FileRequester.cs
@@ -7,8 +7,17 @@
     using System.Threading;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Requester to perform file:// requests.
+    /// </summary>
     public class FileRequester : IRequester
     {
+        /// <summary>
+        /// Performs an asynchronous request that can be cancelled.
+        /// </summary>
+        /// <param name="request">The options to consider.</param>
+        /// <param name="cancel">The token for cancelling the task.</param>
+        /// <returns>The task that will eventually give the response data.</returns>
         public async Task<IResponse> RequestAsync(IRequest request, CancellationToken cancel)
         {
             var requester = FileWebRequest.Create(request.Address.Href) as FileWebRequest;
@@ -29,6 +38,11 @@
             return default(IResponse);
         }
 
+        /// <summary>
+        /// Checks if the given protocol is supported.
+        /// </summary>
+        /// <param name="protocol">The protocol to check for, e.g. file.</param>
+        /// <returns>True if the protocol is supported, otherwise false.</returns>
         public Boolean SupportsProtocol(String protocol)
         {
             return !String.IsNullOrEmpty(protocol) && protocol.Equals(ProtocolNames.File);
