609ec22a1de802bb436b0e2ac0c17965e88697cb
Florian Rappl <florian.rappl@zeiss.com>
2018-12-12 00:46:45 +0100

    Updated dates

--- src/AngleSharp.Io/Properties/AssemblyInfo.cs
+++ src/AngleSharp.Io/Properties/AssemblyInfo.cs
@@ -2,16 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("AngleSharp.Io")]
-[assembly: AssemblyDescription("Extension for AngleSharp to provide additional requesters and storage options.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("AngleSharp")]
-[assembly: AssemblyProduct("AngleSharp.Io")]
-[assembly: AssemblyCopyright("Copyright © AngleSharp, 2013-2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © AngleSharp, 2013-2019")]
 [assembly: ComVisible(false)]
-[assembly: Guid("10d96c84-41d5-4c21-bacf-5d6997c85082")]
-[assembly: AssemblyVersion("0.3.2.0")]
-[assembly: AssemblyFileVersion("0.3.2")]
 [assembly: InternalsVisibleToAttribute("AngleSharp.Io.Tests")]
